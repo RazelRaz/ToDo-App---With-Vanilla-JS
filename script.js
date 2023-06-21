eae54c1bf49f1fac583e794954e1cc36f3819d83
RazelRaz <razel16dcm@gmail.com>
2023-06-21 17:38:44 +0600

    createTask func ready

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const new_task = document.querySelector('#new_task');
 const form = document.querySelector('form');
 const todoUL = document.querySelector('#items');
 const completeUL = document.querySelector('.complete-list ul');
+
+
+const createTask = function(task){
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+
+    label.innerText = task;
+    checkbox.type = 'checkbox';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+
+    return listItem;
+
+}
