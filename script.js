d81c1b1f1a8a649e3c2106ecd0a814579ceebf70
RazelRaz <razel16dcm@gmail.com>
2023-06-21 17:11:49 +0600

    init file ready

---
+++ script.js
@@ -0,0 +1,4 @@
+const new_task = document.querySelector('#new_task');
+const form = document.querySelector('form');
+const todoUL = document.querySelector('#items');
+const completeUL = document.querySelector('.complete-list ul');
